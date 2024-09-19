d2650c0d91105d345abb26b8026170aa371a3c8f
JoachimBrasier <jo-brasier@outlook.com>
2024-09-19 10:16:54 +0200

    Add toaster container

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'sonner';
 
 import { Footer, Navbar } from '@/components/layout';
 
@@ -15,25 +16,19 @@ export const metadata: Metadata = {
   title: 'Forum',
 };
 
-export default async function RootLayout({
-  children,
-  params: { locale },
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
+export default async function RootLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: string } }) {
   const messages = await getMessages();
 
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          defaultTheme={process.env.NEXT_PUBLIC_DEFAULT_THEME ?? 'system'}
-          attribute="class"
-        >
+        <ThemeProvider defaultTheme={process.env.NEXT_PUBLIC_DEFAULT_THEME ?? 'system'} attribute="class">
           <NextIntlClientProvider messages={messages}>
             <Navbar />
-            <main>{children}</main>
+            <main>
+              {children}
+              <Toaster richColors />
+            </main>
             <Footer />
           </NextIntlClientProvider>
         </ThemeProvider>
