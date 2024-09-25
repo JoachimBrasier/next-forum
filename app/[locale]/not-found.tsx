1a98c1860b3a6537f7f164b24d7bb87a3ea0b7ca
JoachimBrasier <jo-brasier@outlook.com>
2024-09-25 15:14:18 +0200

    fix dark mode

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 
+import { Home } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
 
@@ -22,14 +23,15 @@ export default function NotFound() {
 
   return (
     <div className="maxScreenSize flex flex-col items-center justify-center py-10">
-      <h2 className="font-semibold text-indigo-600 text-xl">404</h2>
-      <h3 className="text-black text-5xl font-bold mb-4">{t('title')}</h3>
-      <p className="text-gray-600 text-base font-normal mb-4">{t('description')}</p>
+      <h2 className="font-semibold text-blue-600 text-xl">404</h2>
+      <h3 className="text-gray-700 dark:text-white text-4xl font-bold mb-2">{t('title')}</h3>
+      <p className="text-gray-600 dark:text-gray-400 text-base font-normal mb-4">{t('description')}</p>
       <Link
         href="/"
         aria-label={t('homepageButton.description')}
-        className="flex cursor-pointer flex-row flex-nowrap items-center gap-2 rounded-md border border-indigo-600 bg-indigo-600 px-4 py-2 text-base font-semibold leading-5 text-white hover:border-indigo-500 hover:bg-indigo-500"
+        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
       >
+        <Home className="shrink-0 size-5" />
         {t('homepageButton.text')}
       </Link>
     </div>
