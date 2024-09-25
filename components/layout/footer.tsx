8c4de456bde1f409a70c9505354b744fcfab868f
JoachimBrasier <jo-brasier@outlook.com>
2024-09-25 15:15:03 +0200

    fix dark mode

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default async function Footer() {
   return (
-    <footer className="border-t py-2">
+    <footer className="border-t border-gray-200 dark:border-neutral-700 py-2">
       <div className="maxScreenSize"></div>
     </footer>
   );
