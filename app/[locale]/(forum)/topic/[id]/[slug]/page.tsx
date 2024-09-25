b439a5fbf3fbf129c567e548db55ae8c57ea8475
JoachimBrasier <jo-brasier@outlook.com>
2024-09-25 16:26:47 +0200

    temp remove user avatar to fix build crash

--- app/[locale]/(forum)/topic/[id]/[slug]/page.tsx
+++ app/[locale]/(forum)/topic/[id]/[slug]/page.tsx
@@ -42,16 +42,11 @@ export default async function Category({ params }: Props) {
         <h1 className="text-xl font-bold">{topic.title}</h1>
         <p>TAG</p>
       </div>
-      <div className="flex flex-row flex-nowrap">
-        <div className="mr-4">
-          <Image width={40} height={40} className="rounded-full" src={topic?.user?.image} alt={topic?.user?.name?.charAt(0).toUpperCase()} />
-        </div>
-        <div>
-          <span className="h-10 flex items-center">
-            <h2>{topic?.user?.name ?? 'Deleted user'}</h2>
-          </span>
-          <div>CONTENT</div>
-        </div>
+      <div>
+        <span className="h-10 flex items-center">
+          <h2>{topic?.user?.name ?? 'Deleted user'}</h2>
+        </span>
+        <div>CONTENT</div>
       </div>
       {topic.comments.map((comment) => (
         <div key={comment.id}>{comment.id}</div>
