56435d675bdbb94bf5e053b2657c4f078d105e1b
JoachimBrasier <jo-brasier@outlook.com>
2024-09-25 16:51:46 +0200

    fix ts error

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,12 @@ import Google from 'next-auth/providers/google';
 
 import { prisma } from '@/lib/prisma';
 
+declare module '@auth/core/jwt' {
+  interface JWT {
+    id?: string;
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
@@ -22,7 +28,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return token;
     },
     session({ session, token }) {
-      session.user.id = token.id;
+      if (token.id) {
+        session.user.id = token.id;
+      }
 
       return session;
     },
