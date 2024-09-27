bfd165229ff6903a063962225cf4eb6541e2286d
JoachimBrasier <jo-brasier@outlook.com>
2024-09-27 15:42:07 +0200

    fetch only published topics, fix double border when no topics are rendered

--- app/[locale]/(forum)/category/[slug]/page.tsx
+++ app/[locale]/(forum)/category/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
+import clsx from 'clsx';
 import { getTranslations } from 'next-intl/server';
 
 import Categories from '@/components/common/categories';
@@ -15,8 +16,12 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const category = await prisma.category.findUnique({
-    where: { slug: params.slug },
-    select: { name: true },
+    where: {
+      slug: params.slug,
+    },
+    select: {
+      name: true,
+    },
   });
 
   if (!category) {
@@ -31,12 +36,19 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function Category({ params }: Props) {
   const t = await getTranslations('Forum');
   const category = await prisma.category.findUnique({
-    where: { slug: params.slug },
+    where: {
+      slug: params.slug,
+    },
     include: {
       sub_categories: true,
       tags: true,
       topics: {
-        include: { tag: true },
+        where: {
+          status: 'PUBLISHED',
+        },
+        include: {
+          tag: true,
+        },
       },
     },
   });
@@ -49,8 +61,8 @@ export default async function Category({ params }: Props) {
     <div className="maxScreenSize py-4">
       <h1 className="text-lg mb-4 font-bold">{category.name}</h1>
       {category.sub_categories.length !== 0 && (
-        <div className="border rounded-md mb-4">
-          <h3 className="text-base font-semibold p-2 bg-gray-100">Subcategories</h3>
+        <div className="border rounded-md overflow-hidden mb-4">
+          <h3 className="text-base font-semibold p-2 bg-gray-100 border-b">Subcategories</h3>
           <Categories parentCategory={category.id} />
         </div>
       )}
@@ -60,14 +72,19 @@ export default async function Category({ params }: Props) {
         </Link>
       </div>
       <div className="border rounded-md overflow-hidden divide-y divide-solid">
-        <h3 className="text-base font-semibold p-2 bg-gray-100">Pinned topics</h3>
-        <h3 className="text-base font-semibold p-2 bg-gray-100">Topics</h3>
-        <div className="flex flex-col divide-y divide-solid">
-          {category.topics.map((topic) => (
-            <Link key={topic.id} href={`/topic/${topic.id}/${topic.slug}`} className="text-sm p-2 flex items-center gap-2">
-              {topic.tag && <span className="border bg-gray-100 rounded text-xs px-1 py-0.5">{topic.tag.name}</span>} {topic.title}
-            </Link>
-          ))}
+        <div>
+          <h3 className={clsx('text-base font-semibold p-2 bg-gray-100', { 'border-b': false })}>Pinned topics</h3>
+          <div className="flex flex-col divide-y divide-solid"></div>
+        </div>
+        <div>
+          <h3 className={clsx('text-base font-semibold p-2 bg-gray-100', { 'border-b': category.topics.length !== 0 })}>Topics</h3>
+          <div className="flex flex-col divide-y divide-solid">
+            {category.topics.map((topic) => (
+              <Link key={topic.id} href={`/topic/${topic.id}/${topic.slug}`} className="text-sm p-2 flex items-center gap-2">
+                {topic.tag && <span className="border bg-gray-100 rounded text-xs px-1 py-0.5">{topic.tag.name}</span>} {topic.title}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
