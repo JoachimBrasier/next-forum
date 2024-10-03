a0d6daa2d9d6938cd98dc5c5ad6f328d9200ab4c
JoachimBrasier <jo-brasier@outlook.com>
2024-10-03 16:54:08 +0200

    fix typo style

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -17,7 +17,7 @@ export default async function Navbar() {
   return (
     <nav className="py-2 border-b border-gray-200 dark:border-neutral-700">
       <div className="maxScreenSize flex flex-row flex-nowrap items-center gap-2">
-        <Link href="/" className="mr-auto text-gray-800 dark:text-white">
+        <Link href="/" className="mr-auto text-base font-normal whitespace-nowrap text-gray-800 dark:text-white">
           {process.env?.NEXT_PUBLIC_SITE_NAME || 'Next-forum'}
         </Link>
         <LocalesMenu />
