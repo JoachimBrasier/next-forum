5684ed10b2fe1ae9e32caf22335b943393eb3881
JoachimBrasier <jo-brasier@outlook.com>
2024-10-08 18:37:52 +0200

    fix dropdown crash on page change

--- components/layout/themes-menu.tsx
+++ components/layout/themes-menu.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useIsMounted } from '@/hook/useIsMounted';
+import clsx from 'clsx';
 import { MonitorCog, Moon, Sun } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { useTheme } from 'next-themes';
@@ -10,26 +11,28 @@ export default function ThemesMenu() {
   const { setTheme, resolvedTheme } = useTheme();
   const t = useTranslations('Layout');
 
-  if (!isMounted) {
-    return (
-      <div className="animate-pulse py-2 px-3 inline-flex rounded-lg border border-gray-200 bg-gray-200 shadow-sm outline-none pointer-events-none dark:bg-neutral-700 dark:border-neutral-700">
-        <span className="h-5 w-5" />
-      </div>
-    );
-  }
-
   return (
     <div className="hs-dropdown [--placement:bottom-right] relative inline-flex">
       <button
         id="themes-dropdown"
         type="button"
-        className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+        className={clsx(
+          'hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700',
+          {
+            'animate-pulse border-gray-200 bg-gray-200 dark:bg-neutral-700 dark:border-neutral-700': !isMounted,
+          },
+        )}
         aria-haspopup="menu"
         aria-expanded="false"
         aria-label={t('navbar.themesMenu.openButton.description')}
       >
-        {resolvedTheme === 'light' && <Sun className="shrink-0 size-5" />}
-        {resolvedTheme === 'dark' && <Moon className="shrink-0 size-5" />}
+        {!isMounted ? (
+          <span className="h-5 w-5" />
+        ) : resolvedTheme === 'light' ? (
+          <Sun className="shrink-0 size-5" />
+        ) : (
+          <Moon className="shrink-0 size-5" />
+        )}
       </button>
       <div
         className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700 dark:divide-neutral-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
