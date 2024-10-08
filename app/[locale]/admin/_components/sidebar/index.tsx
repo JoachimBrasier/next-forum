7c20e8e41fe74e16f80a66b2f9d64738b5c1d838
JoachimBrasier <jo-brasier@outlook.com>
2024-10-08 18:22:27 +0200

    fix theme switch transition

--- app/[locale]/admin/_components/sidebar/index.tsx
+++ app/[locale]/admin/_components/sidebar/index.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
   return (
     <div
       id="hs-application-sidebar"
-      className="hs-overlay [--auto-close:lg] hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform w-[260px] h-full hidden fixed inset-y-0 start-0 z-[60] bg-white border-e border-gray-200 lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 dark:bg-neutral-800 dark:border-neutral-700"
+      className="hs-overlay [--auto-close:lg] hs-overlay-open:translate-x-0 -translate-x-full transition-transform duration-300 transform w-[260px] h-full hidden fixed inset-y-0 start-0 z-[60] bg-white border-e border-gray-200 lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 dark:bg-neutral-800 dark:border-neutral-700"
       role="dialog"
       tabIndex={-1}
       aria-label="Sidebar"
